ced49fee559318430de24ec30c443e705235cd16
drjoeycadieux <instructor.joeycadieux@gmail.com>
2022-01-21 09:24:57 -0500

    inm

---
+++ weather/pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Severe Storms Media</title>
+        <meta name="description" content="Severe Storms Media | Your source for weather breaking media" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+       <h2>
+         Severe Storms Media
+       </h2>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
